ea744a072ebfe3b7341bfdda94de0894b7200f08
Francisco Núñez Sierra <franusierra@gmail.com>
2025-07-26 17:42:39 +0200

    fix(infra): add tfvars.prod

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,7 +11,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region."
   type        = string
-  default     = "westeurope"
+  default     = "spaincentral"
 }
 
 variable "static_webapp_name" {
